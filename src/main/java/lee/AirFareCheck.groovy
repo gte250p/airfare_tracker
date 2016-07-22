34aa9fb5c142d526c04a28fab5cea41d64c322a3
Brad Lee <s.brad.lee@gmail.com>
2016-07-22 15:17:53 -0400

    Updated to use chrome driver (instead of firefox due to home machine errors).  Arguments work now

--- src/main/java/lee/AirFareCheck.groovy
+++ src/main/java/lee/AirFareCheck.groovy
@@ -1,12 +1,16 @@
 package lee
 
 import org.openqa.selenium.*
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.support.ui.ExpectedCondition
 import org.openqa.selenium.support.ui.WebDriverWait
 
 public class AirFareCheck {
 
+    static Integer MAX_COUNT = 20;
     static String departDate = "10/15/2016";
     static String returnDate = "10/23/2016";
 
@@ -14,41 +18,40 @@ public class AirFareCheck {
         if( args?.length > 0 ){
             for( String arg : args ){
                 if( arg.toLowerCase().startsWith("--departs=") ){
-                    departDate =
+                    departDate = arg.substring("--departs=".length());
+                }else if( arg.toLowerCase().startsWith("--count=") ){
+                    MAX_COUNT = Integer.parseInt(arg.substring("--count=".length()));
+                }else if( arg.toLowerCase().startsWith("--returns=") ) {
+                    returnDate = arg.substring("--returns=".length());
                 }
             }
         }
 
-        String EXPEDIA_URL = "https://www.expedia.com/Flights-Search?trip=roundtrip&leg1=from:ATL,to:OGG,departure:${departDate}TANYT&leg2=from:OGG,to:ATL,departure:${returnDate}TANYT&passengers=children:0,adults:2,seniors:0,infantinlap:Y&mode=search";
+        Calendar now = Calendar.getInstance();
 
+        String URL = "https://www.expedia.com/Flights-Search?trip=roundtrip&leg1=from:ATL,to:OGG,departure:${departDate}TANYT&leg2=from:OGG,to:ATL,departure:${returnDate}TANYT&passengers=children:0,adults:2,seniors:0,infantinlap:Y&mode=search";
+//        String URL = "https://www.kayak.com/flights/ATL-OGG/${departDate}/${returnDate}/2adults"
 
-        WebDriver driver = new FirefoxDriver();
-        driver.get(EXPEDIA_URL);
+        def options = new ChromeOptions()
+        options.addArguments("--start-maximized")
+        WebDriver driver = new ChromeDriver(options);
+        println "Loading URL: "+URL
+        driver.get(URL);
 
-        System.out.println("Page title is: " + driver.getTitle());
-
-        try {
-            (new WebDriverWait(driver, 30)).until(new ExpectedCondition<Boolean>() {
-                public Boolean apply(WebDriver d) {
-                    return d.findElement(By.id("stopsFilterList")).isDisplayed();
-                }
-            });
-            System.out.println("Successfully found stopsFilterList")
-        }catch(Throwable t){
-            errorAndDie("Timed out waiting for stopsFilterList!", t);
-        }
+        Thread.sleep(15000); // Sleep for 15 seconds, period (all data should load during this time)
 
         println("Finding all flight-module...");
         List<WebElement> flightModules = driver.findElements(By.cssSelector("article.flight-module"));
         int flightModuleCount = 0;
         for( WebElement we : flightModules ){
-            if( flightModuleCount > 20 )
+            if( flightModuleCount > MAX_COUNT )
                 break;
             try {
                 String price = we.findElement(By.cssSelector("div.price-column")).getAttribute("data-test-price-per-traveler")
                 String departureTime = we.findElement(By.cssSelector("span.departure-time")).getText()?.trim()
                 String arrivalTime = we.findElement(By.cssSelector("span.arrival-time")).getText()?.trim()
-                println("Found Flight! price: " + price +", departs: "+departureTime+", arrives: "+arrivalTime)
+                String duration = we.findElement(By.cssSelector("div.duration-emphasis")).getText()?.trim()
+                println("Found Flight! price: " + price +", departs: "+departureTime+", arrives: "+arrivalTime+", duration: "+duration)
                 flightModuleCount++;
             }catch(Throwable t){
                 System.err.println("Error finding price: "+t);
@@ -59,7 +62,7 @@ public class AirFareCheck {
         //Close the browser
         driver.quit();
 
-        System.out.println("Succesfully Executed.");
+        System.out.println("Successfully Executed.");
 
 
     }
